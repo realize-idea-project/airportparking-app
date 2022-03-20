0c68db2ab3f604e96be5b23561583ea045ba9371
han <dev.ryu2021@gmail.com>
2022-03-20 17:52:52 +0900

    Delete navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import Config from 'react-native-config';
-import { StyleSheet } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
+import { StyleSheet, SafeAreaView } from 'react-native';
 
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
@@ -13,46 +12,9 @@ const Tab = createBottomTabNavigator();
 const App = () => {
   console.log(Config.ENV);
   return (
-    <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen
-          name="DailyChart"
-          component={DailyChart}
-          options={{
-            tabBarActiveBackgroundColor: 'rgb(44, 61, 105)',
-            tabBarActiveTintColor: 'white',
-            headerShown: false,
-            tabBarLabel: '일일 주차 목록',
-            tabBarLabelStyle: {
-              fontSize: 20,
-              alignContent: 'center',
-              justifyContent: 'center',
-              marginBottom: 10,
-              fontWeight: '700',
-            },
-            tabBarIcon: () => null,
-          }}
-        />
-        <Tab.Screen
-          name="ServiceIn"
-          component={ServiceIn}
-          options={{
-            tabBarActiveBackgroundColor: 'rgb(44, 61, 105)',
-            tabBarActiveTintColor: 'white',
-            headerShown: false,
-            tabBarLabel: '입고 문자',
-            tabBarLabelStyle: {
-              fontSize: 20,
-              alignContent: 'center',
-              justifyContent: 'center',
-              marginBottom: 10,
-              fontWeight: '700',
-            },
-            tabBarIcon: () => null,
-          }}
-        />
-      </Tab.Navigator>
-    </NavigationContainer>
+    <SafeAreaView>
+      <DailyChart />
+    </SafeAreaView>
   );
 };
 
