30223c6d3578c97ff5040b91d7f2e15a24f3df63
han <dev.ryu2021@gmail.com>
2022-03-26 15:26:20 +0900

    Change navigation structure

--- src/screens/screens.tsx
+++ src/screens/screens.tsx
@@ -3,38 +3,48 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import DailyChart from './Dailychart/Dailychart';
+import { DatePicker } from './Dailychart/DatePicker';
+import _ from 'lodash';
 
 interface ScreenDefinition {
   id: string;
-  Stack: any;
-  screenOptions?: any;
   routes: Screen[];
 }
-
 interface Screen {
   name: string;
   component: FC<any>;
   options?: any;
 }
 
-const DailyChartScreenDefinition: ScreenDefinition = {
+const DailyChartScreenDefinition = {
   id: 'DailyChart',
-  Stack: createNativeStackNavigator(),
-  screenOptions: { headerShown: false },
-  routes: [{ name: 'DailyChart', component: DailyChart, options: {} }],
+  routes: [
+    { name: 'DailyChart', component: DailyChart, options: {} },
+    { name: 'DatePicker', component: DatePicker, options: {} },
+  ],
 };
 
-const screenDefinitions = [DailyChartScreenDefinition];
+const screenDefinitions: ScreenDefinition[] = [DailyChartScreenDefinition];
+
+const AppNavigation = {
+  Stack: createNativeStackNavigator(),
+  navigatorOptions: {
+    screenOptions: { headerShown: false },
+    initialRouteName: 'DatePicker',
+  },
+  routes: _.flatMap(screenDefinitions, ({ routes }) => routes),
+};
+
 const AppNavigationProvider = () => {
+  const { Stack, navigatorOptions, routes } = AppNavigation;
+
   return (
     <NavigationContainer>
-      {screenDefinitions.map(({ id, Stack, routes, screenOptions }) => (
-        <Stack.Navigator key={id} screenOptions={screenOptions}>
-          {routes.map(({ name, component, options }) => (
-            <Stack.Screen key={name} name={name} component={component} options={options} />
-          ))}
-        </Stack.Navigator>
-      ))}
+      <Stack.Navigator {...navigatorOptions}>
+        {routes.map(({ name, component, options }) => (
+          <Stack.Screen key={name} name={name} component={component} options={options} />
+        ))}
+      </Stack.Navigator>
     </NavigationContainer>
   );
 };
