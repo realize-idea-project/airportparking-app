5afad59d995715f578490b822078c83c2461a61a
han <dev.ryu2021@gmail.com>
2022-04-08 22:52:07 +0900

    Send SMS to ServiceOut users

---
+++ src/screens/dailychart/constants.ts
@@ -0,0 +1,13 @@
+export const alertMessages = {
+  noReservation: '해당 날짜에 예약이 없습니다. 날짜를 확인해주세요.',
+  doneSavingMobiles: '번호 저장이 완료되었습니다.',
+  sendServiceInMessageTitle: '<입고>',
+  sendServiceInMessage: '입고 손님에게 문자를 보내시겠습니까?',
+  sendServiceOutMessageTitle: '<출고>',
+  sendServiceOutMessage: '출고 손님에게 문자를 보내시겠습니까?',
+};
+
+export const globalTextString = {
+  cancel: '취소',
+  send: '보내기',
+};
